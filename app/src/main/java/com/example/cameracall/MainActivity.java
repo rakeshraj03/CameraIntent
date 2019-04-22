520f904df0b3008811f60a6252c33c7e2b9e1324
rakesh raj <grakesh0301@gmail.com>
2019-04-23 03:12:11 +0530

    Url update

--- app/src/main/java/com/example/cameracall/MainActivity.java
+++ app/src/main/java/com/example/cameracall/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
 
         final String ConvertImage = Base64.encodeToString(byteArrayVar, Base64.DEFAULT);
 
-        String loginPostUrl = "http://eatsmartai.com:3000/predict/";
+        String loginPostUrl = "http://eatsmartai.com:5000/predict";
         OkHttpLoginHandler loginBgTask = new OkHttpLoginHandler();
        // loginBgTask.execute(loginPostUrl);
 
